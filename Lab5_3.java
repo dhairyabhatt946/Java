3e26522544e5e0186e2bdd745aa884055e4bf62e
dhairyabhatt946 <143011610+dhairyabhatt946@users.noreply.github.com>
2024-02-16 11:28:35 +0530

    Add files via upload

---
+++ Lab5_3.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Vowel{
+    String str;
+    int count_a,count_e,count_i,count_o,count_u;
+    Vowel(String str){
+        this.str = str;
+    }
+    public void countEachVowel(String str){
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == 'a') {
+                count_a++;
+            }
+            else if(str.charAt(i) == 'e') {
+                count_e++;
+            }
+            else if(str.charAt(i) == 'i') {
+                count_i++;
+            }
+            else if(str.charAt(i) == 'o') {
+                count_o++;
+            }
+            else if(str.charAt(i) == 'u') {
+                count_u++;
+            }
+        }
+        System.out.println("a = "+count_a+" e = "+count_e+" i = "+count_i+" o = "+count_o+" u = "+count_u);
+    }
+}
+public class Lab5_3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str;
+        while (true) {
+            str = sc.nextLine().toLowerCase();
+            if(str.equals("quit")) break;
+            Vowel v = new Vowel(str);
+            v.countEachVowel(str);
+        }
+    }
+}
---
+++ Lab5_4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Bank_Account {
+    /*accountNo, userName, email, accountType and accountBalance, */
+    int accountNo;
+    String userName = new String();
+    String email = new String();
+    String accountType = new String();
+    int accountBalance;
+    
+}
+public class Lab5_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many account details do you want to input?");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+    }
+}
