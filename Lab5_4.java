7764d7af4898fb28f8655e649cfc1424e1a6fe9d
dhairyabhatt946 <143011610+dhairyabhatt946@users.noreply.github.com>
2024-02-17 17:24:20 +0530

    Update Lab5_4.java

--- Lab5_4.java
+++ Lab5_4.java
@@ -7,6 +7,28 @@ class Bank_Account {
     String email = new String();
     String accountType = new String();
     int accountBalance;
+    public void getAccountDetails(int i) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter details for account "+i);
+        System.out.println("Enter account number");
+        accountNo = sc.nextInt();
+        System.out.println("Enter username");
+        userName = sc.next();
+        System.out.println("Enter email");
+        email = sc.next();
+        System.out.println("Enter account type");
+        accountType = sc.next();
+        System.out.println("Enter account balance");
+        accountBalance = sc.nextInt();
+    }
+    public void displayAccountDetails(int i) {
+        System.out.println("Account "+i);
+        System.out.println("Account Number: "+accountNo);
+        System.out.println("Username: "+userName);
+        System.out.println("email: "+email);
+        System.out.println("Account type: "+accountType);
+        System.out.println("Account Balance: "+accountBalance);
+    }
     
 }
 public class Lab5_4 {
@@ -14,6 +36,15 @@ public class Lab5_4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many account details do you want to input?");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        Bank_Account[] account_Details = new Bank_Account[n];
+        for(int i=0; i<account_Details.length; i++) {
+            account_Details[i] = new Bank_Account();
+        }
+        for(int i=0; i<account_Details.length; i++) {
+            account_Details[i].getAccountDetails(i+1);
+        }
+        for(int i=0; i<account_Details.length; i++) {
+            account_Details[i].displayAccountDetails(i+1);
+        }
     }
 }
